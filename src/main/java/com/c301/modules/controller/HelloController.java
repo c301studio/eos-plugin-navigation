980c438ab2d070d715f89b00b761e834fbad68ed
Chenbing <muzi@muzilib.com>
2023-01-04 17:20:03 +0800

    chore(swagger相关): 测试swagger

--- src/main/java/com/c301/modules/controller/HelloController.java
+++ src/main/java/com/c301/modules/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.c301.modules.controller;
 
 import com.c301.modules.plugin.base.annotation.ApiVersion;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
@@ -18,17 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2022/12/28 15:08
  * @Version 1.0
  **/
+@Api(tags = "插件接口")
 @Controller
 @ApiVersion(value = "/ap1")
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
+    @ApiOperation("hello-world")
     @GetMapping("/hello-world")
     public String hello() {
         return "Hello world";
     }
 
+    @ApiOperation("说话")
     @GetMapping("/{say}")
     public String say(@PathVariable("say") String say) {
         return say;
